0cf9e8db01434199b65c0436dbb4c96493b8bd51
wilsonwp <wperez@dd-wp.com>
2016-01-13 12:19:37 -0500

    cambios controller comentarios

--- app/Http/Controllers/ComentariosController.php
+++ app/Http/Controllers/ComentariosController.php
@@ -55,6 +55,7 @@ class ComentariosController extends Controller
                                         </a>
                                         <div class="clear">
                                           <p>'.$request['contenido'].'</p>
+                                          
                                         </div>
                                       </div>
                                     </div>
@@ -238,6 +239,73 @@ class ComentariosController extends Controller
                     break;
              endswitch;
 
+           /* switch ($request['tipo_comentario_id']):
+                case 0 :
+                    $request['look'] = '<li class="tl-item" ng-repeat="comentario in comentarios">
+                                <div class="tl-wrap b-info">
+                                <span class="tl-date">'.$request['minuto'].'</span>
+                                <div class="tl-content">
+                                '.$request['contenido'].'
+                                 </div>
+                                 </div>
+                                 </li>';
+                    break;
+                case 1 :
+                     $request['look'] = '<li class="tl-item" ng-repeat="comentario in comentarios">
+                                <div class="tl-wrap b-info">
+                                <span class="tl-date">'.$request['minuto'].'</span>
+                                <div class="tl-content">
+                                '.$request['contenido'].'
+                                 </div>
+                                 </div>
+                                 </li>';
+                    break;
+                case 2 :
+                     $request['look'] = '<li class="tl-item">
+                                         <div class="tl-wrap">
+                                          <span class="tl-date">'.$request['minuto'].'</span>
+                                          <div class="tl-content panel padder b-a">
+                                          <span class="arrow left pull-up"></span>
+                                           <div>'.$request['contenido'].'</div>
+                                            </div>
+                                             </div>
+                                             </li>';
+                    break;
+                case 3 :
+                     $request['look'] = '<li class="tl-item tl-left">
+                                              <div class="tl-wrap b-success">
+                                           <span class="tl-date">'.$request['minuto'].'/span>
+                                            <div class="tl-content panel bg-success padder">
+                                            <span class="arrow arrow-success left pull-up hidden-left"></span>
+                                           <span class="arrow arrow-success right pull-up visible-left"></span>
+                                            <div class="text-lt">'.$request['contenido'].'</div>
+                                            </div>
+                                           </div>
+                                             </li>';
+                    break;
+                case 4 :
+                     $request['look'] = '  <li class="tl-item">
+                                            <div class="tl-wrap b-primary">
+                                            <span class="tl-date">'.$request['minuto'].'</span>
+                                             <div class="tl-content panel padder b-a w-md w-auto-xs">
+                                                <span class="arrow left pull-up"></span>
+                                                <div class="text-lt m-b-sm">'.$request['titulo'].'</div>
+                                                <div class="panel-body pull-in b-t b-light">
+                                                  <a href="" class="thumb pull-right m-l m-t-xs avatar">
+                                                    <img src="img/a4.jpg" alt="...">
+                                                    <i class="on md b-white bottom"></i>
+                                                  </a>
+                                                  <div class="clear">
+                                                    <a href="" class="text-primary block m-b-xs">@Mike Mcalidek <i class="icon-twitter"></i></a>
+                                                    
+                                                  </div>
+                                                </div>
+                                              </div>
+                                            </div>
+                                          </li>
+    ';
+                    break;
+             endswitch;*/
        
            Comentario::create($request->all());
            return response()->json([
